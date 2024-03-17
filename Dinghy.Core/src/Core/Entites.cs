e27b13bb85855a2103dce86d3a4c5c7ff9c7cf77
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2024-03-17 08:38:35 -0700

    fix particle emitter

--- Dinghy.Core/src/Core/Entites.cs
+++ Dinghy.Core/src/Core/Entites.cs
@@ -500,8 +500,21 @@ public class ParticleEmitter : Entity
     public ParticleEmitter(ParticleEmitterComponent.EmitterConfig config, Scene? scene = null, bool startEnabled = true) : base(startEnabled,scene)
     {
         Config = config;
+        sceneRenderOrder = Scene.GetNextSceneRenderCounter();
         ECSEntity.Add(
-            new RenderItem(scene.GetNextSceneRenderCounter()),
+            new RenderItem(sceneRenderOrder),
             new ParticleEmitterComponent(config));
+    }
+    
+    private int sceneRenderOrder;
+    public int SceneRenderOrder
+    {
+        get => sceneRenderOrder;
+        set
+        {
+            ref var r = ref ECSEntity.Get<RenderItem>();
+            r.renderOrder = value;
+            sceneRenderOrder = value;
+        }
     }
 }
